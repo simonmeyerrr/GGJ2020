8eb89e0676f821efc55f72a8d493c532552e7b5d
Kevin <kevin.spegt@epitech.eu>
2020-01-31 23:55:01 +0100

    [+] Using s_ptr

---
+++ src/Utils/Using.hpp
@@ -0,0 +1,20 @@
+/*
+**
+** GLOBAL GAME JAM
+**
+*/
+
+#ifndef USING_HPP
+    #define USING_HPP
+
+    #include <iostream>
+    #include <memory>
+    #include <string>
+
+template <typename T>
+using u_ptr = std::unique_ptr<T>;
+template <typename T>
+using s_ptr = std::shared_ptr<T>;
+using suint = short unsigned int;
+
+#endif /* USING_HPP */
