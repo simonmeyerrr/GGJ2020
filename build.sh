03c32cf8018bac52340308e6fba33573c3461c78
Simon Meyer <simon.meyer@epitech.eu>
2020-01-31 22:14:51 +0100

    add include and ddls copy

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-echo "Creating build directory" && mkdir -p build && cd build && \
-echo "Generating Makefile" && cmake .. -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" && \
-echo "Building project" && cmake --build .
+echo "Os type:" $OSTYPE
+
+if [[ "$OSTYPE" == "win32" ]]; then
+  echo "Use visual studio to build this project by your own"
+else
+  echo "Creating build directory" && mkdir -p build && cd build && \
+  echo "Generating Makefile" && cmake .. -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" && \
+  echo "Building project" && cmake --build .
+fi
