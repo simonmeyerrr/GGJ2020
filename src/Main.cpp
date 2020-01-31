26f212f085adcda1a063f6c7c549211ecd4cfa81
Simon Meyer <simon.meyer@epitech.eu>
2020-01-31 22:52:44 +0100

    Works on windows

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,25 @@
-#include <iostream>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
 
 int main(int const ac, char const *const *av, char const *const *env)
 {
-    std::cout << "This game works fine !" << std::endl;
-
+	// Create the main window
+	sf::RenderWindow window(sf::VideoMode(800, 600), "GGJ2020");
+	// Start the game loop
+	while (window.isOpen())
+	{
+		// Process events
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// Close window: exit
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		// Clear screen
+		window.clear();
+		// Update the window
+		window.display();
+	}
     return 0;
 }
