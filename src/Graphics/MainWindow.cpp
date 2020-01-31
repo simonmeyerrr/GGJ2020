be1501c7446e664e06bc42a3cf2725734fc6026c
Kevin <kevin.spegt@epitech.eu>
2020-02-01 00:11:14 +0100

    [*] Graphics/MainWindow: modify 035

--- src/Graphics/MainWindow.cpp
+++ src/Graphics/MainWindow.cpp
@@ -1,18 +1,22 @@
+/*
+**
+** GLOBAL GAME JAM
+**
+*/
+
 #include "MainWindow.hpp"
 
-MainWindow::MainWindow() :
-_renderWindow(std::make_shared<sf::RenderWindow>(
-        sf::VideoMode(1600,
-                900,
-                32), "GGJ20")) {
+MainWindow::MainWindow()
+    : _renderWindow(std::make_shared<sf::RenderWindow>(
+        sf::VideoMode(1600, 900, 32), "GGJ20")) {
 }
 
-MainWindow::~MainWindow() = default;
-
-sf::Vector2i MainWindow::getMousePosition() const {
+sf::Vector2i MainWindow::getMousePosition() const
+{
     return sf::Mouse::getPosition(*_renderWindow);
 }
 
-void MainWindow::setMousePosition(sf::Vector2i pos) {
+void MainWindow::setMousePosition(sf::Vector2i pos)
+{
     _posMouse = sf::Mouse::getPosition(*_renderWindow);
 }
--- src/Graphics/MainWindow.hpp
+++ src/Graphics/MainWindow.hpp
@@ -1,19 +1,32 @@
-#include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/Window/Mouse.hpp>
-#include <memory>
+/*
+**
+** GLOBAL GAME JAM
+**
+*/
+
+#ifndef MAINWINDOW_HPP
+    #define MAINWINDOW_HPP
+
+    #include <SFML/Graphics/RenderWindow.hpp>
+    #include <SFML/Window/Mouse.hpp>
+    #include <memory>
 
 class MainWindow {
 public:
+    // Constructors
     MainWindow();
     MainWindow(MainWindow const &) = delete;
-    ~MainWindow();
-
     MainWindow &operator=(MainWindow const &) = delete;
+    ~MainWindow() = default;
 
+    // Setters & Getters
     sf::Vector2i getMousePosition() const;
-    void setMousePosition(sf::Vector2i pos);
+    void setMousePosition(sf::Vector2i);
 
 private:
+    // Variables
     std::shared_ptr<sf::RenderWindow> _renderWindow;
     sf::Vector2i _posMouse;
 };
+
+#endif /* MAINWINDOW_HPP */
