11c49d240f8570055e40727d6dd290c421a3687a
Kevin <kevin.spegt@epitech.eu>
2020-02-02 15:29:54 +0100

    [+] Intro

--- src/Scene/Intro.cpp
+++ src/Scene/Intro.cpp
@@ -12,10 +12,9 @@
 Intro::Intro(Saves &save)
     : AScene(SCENE_INTRO, save),_updates(0)
 {
-    _gameObject[0] = std::make_shared<StaticGameObject>("./assets/textures/classroom.png", sf::IntRect{0, 0, 1600, 900});
-    _gameObject[1] = std::make_shared<StaticGameObject>("./assets/textures/coridor.png", sf::IntRect{0, 0, 1600, 900});
-    _gameObject[2] = std::make_shared<StaticGameObject>("./assets/textures/classroom.png", sf::IntRect{0, 0, 1600, 900});
-    _gameObject[3] = std::make_shared<StaticGameObject>("./assets/textures/coridor.png", sf::IntRect{0, 0, 1600, 900});
+    _gameObject[0] = std::make_shared<StaticGameObject>("./assets/textures/intro1.png", sf::IntRect{0, 0, 1600, 900});
+    _gameObject[1] = std::make_shared<StaticGameObject>("./assets/textures/intro2.png", sf::IntRect{0, 0, 1600, 900});
+    _gameObject[2] = std::make_shared<StaticGameObject>("./assets/textures/intro3.png", sf::IntRect{0, 0, 1600, 900});
     _uiObject[0] = std::make_shared<Fade>();
     dynamic_cast<Fade &>(*(_uiObject[0])).start(sf::Color::Black, 100, false);
 }
@@ -24,10 +23,12 @@ IScene::Event Intro::update()
 {
     _uiObject[0]->update();
     ++_updates;
-    if (_updates >= 800)
+    if (_updates >= 900)
         return Event{EVENT_PUSH_SCENE, SCENE_MAIN_MENU};
-    if (_updates == 700)
-        dynamic_cast<Fade &>(*(_uiObject[0])).start(sf::Color::Black, 100, true);
+    if (_updates == 280 || _updates == 580 || _updates == 860)
+        dynamic_cast<Fade &>(*(_uiObject[0])).start(sf::Color::Black, 40, true);
+    if (_updates == 300 || _updates == 600 || _updates == 900)
+        dynamic_cast<Fade &>(*(_uiObject[0])).start(sf::Color::Black, 30, false);
     return Event{EVENT_NONE, SCENE_INTRO};
 }
 
@@ -38,14 +39,12 @@ IScene::Event Intro::event(sf::RenderWindow &win, sf::Event &event)
 
 void Intro::display(sf::RenderWindow &win, shaders_map &)
 {
-    if (_updates < 200)
+    if (_updates < 299)
         win.draw(_gameObject[0]->getSprite());
-    else if (_updates < 400)
+    else if (_updates > 299 && _updates < 600)
         win.draw(_gameObject[1]->getSprite());
-    else if (_updates < 600)
+    else if (_updates > 599 && _updates < 900)
         win.draw(_gameObject[2]->getSprite());
-    else
-        win.draw(_gameObject[3]->getSprite());
     _uiObject[0]->draw(win);
 }
 
